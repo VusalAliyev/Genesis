769adaa7770bdd676f8f41c652029120f59bbd5b
Vusal Aliyev <vusalaliev02@gmail.com>
2023-05-07 12:36:21 +0400

    Add project files.

---
+++ src/Genesis.Infrastructure/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Genesis.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+        
+    }
+}
