16a4998ae94e1c1316f34aab7c895daa6832b0ad
Hüseyn Alıyev <huseynma@code.edu.az>
2023-05-07 15:20:35 +0400

    Added OTPService

---
+++ src/Genesis.Infrastructure/Services/EmailService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Genesis.Infrastructure.Services;
+
+public static class EmailService
+{
+    public static void SendEmail(string toWho, string content)
+    {
+        string from = "aliev.hsyn@gmail.com";
+        string to = toWho;
+        string subject = "Confirm you email";
+        string body = content;
+
+        MailMessage mailMessage = new(
+            from,
+            to,
+            subject,
+            body
+        );
+        mailMessage.BodyEncoding = System.Text.Encoding.UTF8;
+        mailMessage.IsBodyHtml = true;
+
+        SmtpClient client = new("smtp.gmail.com", 587);
+        client.EnableSsl = true;
+        client.UseDefaultCredentials = false;
+        client.Credentials = new NetworkCredential(from, "vktvykutjpgmwvyz");
+        try
+        {
+            client.Send(mailMessage);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+}
---
+++ src/Genesis.WebApi/Controllers/OTPController.cs
@@ -0,0 +1,43 @@
+﻿using Genesis.Infrastructure.Dto;
+using Genesis.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Genesis.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OTPController : ControllerBase
+    {
+        [HttpPost("getOtp")]
+        public IActionResult GetOtp(OTPConfirmation confirmation)
+        {
+            Random generator = new Random();
+            string otp = generator.Next(0, 1000000).ToString("D4");
+            string otpContent = $"<h1>OTP: {otp}</h1>";
+            EmailService.SendEmail(confirmation.Email, otpContent);
+            Response.Cookies.Append($"{confirmation.FinCode}", otp);
+
+            return Ok(otp);
+        }
+
+        [HttpPost("confirmOtp")]
+        public IActionResult ConfirmOtp(string otp, string finCode)
+        {
+            string otpCode = Request.Cookies[$"{finCode}"];
+
+            if (otpCode == null)
+            {
+                throw new Exception();
+            }
+
+            if (otp == otpCode)
+            {
+                return Ok("Success");
+            }
+            else
+            {
+                throw new Exception("Code is not correct!");
+            }
+        }
+    }
+}
