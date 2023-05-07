6c546f3daed95ee4ed01c3500756c131b755e52c
Vusal Aliyev <vusalaliev02@gmail.com>
2023-05-07 15:38:37 +0400

    ServiceErrorContinued

---
+++ src/Genesis.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using Genesis.Application.Features.Commands.Customer.CreateCustomer;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Genesis.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class CustomerController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CustomerController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCustomerCommandRequest requestModel)
+        {
+            CreateCustomerCommandResponse customer=await _mediator.Send(requestModel);
+            return Ok(customer);
+        }
+    }
+}
