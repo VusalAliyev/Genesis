20cc1ec0b7a6ad7c0244654dd77c094a3c24a539
nadir <nadir@MACHINE>
2023-05-07 14:30:05 +0400

    added mapper

---
+++ src/Genesis.Application/Common/Mapping/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Genesis.Application.Features.Queries.User.GetUser;
+using Genesis.Domain.Entities;
+
+namespace Genesis.Application.Common.Mapping
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, GetUserQueryResponse>().ReverseMap();
+        }
+    }
+}
